3c8c130f0d37f89c8d17490e252ec1ae8968616c
Ruksana <ruksana01official@gmail.com>
2023-11-23 19:58:40 +0600

    add role for users (part-3)

--- hotel-booking-client/src/main.jsx
+++ hotel-booking-client/src/main.jsx
@@ -2,18 +2,23 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import AuthProvider from './providers/AuthProvider'
-
 import { Toaster } from 'react-hot-toast'
 import 'react-date-range/dist/styles.css'
 import 'react-date-range/dist/theme/default.css'
 import { HelmetProvider } from 'react-helmet-async'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { router } from './router/Routes'
+// import React from 'react'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HelmetProvider>
     <AuthProvider>
-      <Toaster />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </AuthProvider>
   </HelmetProvider>
 )
--- hotel-booking-server/index.js
+++ hotel-booking-server/index.js
@@ -3,7 +3,7 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const jwt = require('jsonwebtoken')
 const morgan = require('morgan')
 const port = process.env.PORT || 5000
@@ -60,23 +60,24 @@ const client = new MongoClient(process.env.DB_URI, {
 async function run() {
   try {
     const usersCollection = client.db('hotelBookingDB').collection('users')
-       // auth related api
-       app.post('/jwt', async (req, res) => {
-        const user = req.body
-        console.log('I need a new jwt', user)
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-          expiresIn: '365d',
-        })
-        res
-          .cookie('token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-          })
-          .send({ success: true })
+    const roomsCollection = client.db('hotelBookingDB').collection('rooms')
+    // auth related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      console.log('I need a new jwt', user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
       })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
 
-        // Logout
+    // Logout
     app.get('/logout', async (req, res) => {
       try {
         res
@@ -93,24 +94,61 @@ async function run() {
     })
 
 
-        // Save or modify user email, status in DB
-        app.put('/users/:email', async (req, res) => {
-          const email = req.params.email
-          const user = req.body
-          const query = { email: email }
-          const options = { upsert: true }
-          const isExist = await usersCollection.findOne(query)
-          console.log('User found?----->', isExist)
-          if (isExist) return res.send(isExist)
-          const result = await usersCollection.updateOne(
-            query,
-            {
-              $set: { ...user, timestamp: Date.now() },
-            },
-            options
-          )
-          res.send(result)
-        })
+    // Save or modify user email, status in DB
+    app.put('/users/:email', async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const query = { email: email }
+      const options = { upsert: true }
+      const isExist = await usersCollection.findOne(query)
+      console.log('User found?----->', isExist)
+      if (isExist) return res.send(isExist)
+      const result = await usersCollection.updateOne(
+        query,
+        {
+          $set: { ...user, timestamp: Date.now() },
+        },
+        options
+      )
+      res.send(result)
+    })
+
+    // Get user role
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email
+      const result = await usersCollection.findOne({ email })
+      res.send(result)
+    })
+    
+// Save a room in database
+    app.post('/rooms', verifyToken, async (req, res) => {
+      const room = req.body
+      const result = await roomsCollection.insertOne(room)
+      res.send(result)
+    })
+
+    // Get all rooms
+    app.get('/rooms', async (req, res) => {
+      const result = await roomsCollection.find().toArray()
+      res.send(result)
+    })
+
+    //get rooms for host
+    app.get('/rooms/:email', async (req, res) => {
+      const email = req.params.email
+      const result = await roomsCollection
+        .find({ 'host.email': email })
+        .toArray()
+      res.send(result)
+    })
+
+    // Get single room data
+    app.get('/room/:id', async (req, res) => {
+      const id = req.params.id
+      const result = await roomsCollection.findOne({ _id: new ObjectId(id) })
+      res.send(result)
+    })
+
     
 
     // await client.connect();
