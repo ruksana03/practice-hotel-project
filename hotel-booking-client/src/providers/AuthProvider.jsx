99aaac277d517e0d0b5e768b0b0c4f0053420b35
Ruksana <ruksana01official@gmail.com>
2023-11-23 13:43:26 +0600

    add server folder (part-2)

--- hotel-booking-client/src/providers/AuthProvider.jsx
+++ hotel-booking-client/src/providers/AuthProvider.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import { createContext, useEffect, useState } from 'react'
-// import {} from 'fire'
 import {
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
@@ -13,6 +12,7 @@ import {
   updateProfile,
 } from 'firebase/auth'
 import { app } from '../firebase/firebase.config'
+import { clearCookie } from '../api/auth'
 
 export const AuthContext = createContext(null)
 const auth = getAuth(app)
@@ -42,8 +42,9 @@ const AuthProvider = ({ children }) => {
     return sendPasswordResetEmail(auth, email)
   }
 
-  const logOut = () => {
+  const logOut = async () => {
     setLoading(true)
+    await clearCookie()
     return signOut(auth)
   }
 
